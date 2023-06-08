4b1e7e908f0cf6dbaad5133a17ce0c909e4a63d4
Bladen Sawatsky <s0gs2004@gmail.com>
2023-06-08 12:45:08 -0700

    updates

--- _config.h
+++ _config.h
@@ -3,18 +3,11 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
-static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
-static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
-static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
-static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"Source Code Pro Semibold:style=Semibold:size=12", "Font Awesome 6 Free:style=Regular:size=12"};
 static const char dmenufont[]       = "monospace:size=16";
 //SourceCode-Bold
-static const unsigned int baralpha = 0xd0;
-static const unsigned int borderalpha = OPAQUE;
 static const char col_gray1[]       = "#0d0218";
 static const char col_gray2[]       = "#33075f";
 static const char col_gray3[]       = "#d70f73";
@@ -24,11 +17,6 @@ static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
-};
-static const unsigned int alphas[][3]      = {
-	/*               fg      bg        border     */
-	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
-	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
@@ -111,22 +99,6 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
-	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
-	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
-	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
-	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
-	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
-	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
-	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
-	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
-	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
-	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
--- config.h
+++ config.h
@@ -3,11 +3,20 @@
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int snap      = 10;       /* snap pixel */
+static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
+static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
+static const unsigned int gappoh    = 20;       /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const int vertpad            = 10;       /* vertical padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
+static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = {"Source Code Pro Semibold:style=Semibold:size=12", "Font Awesome 6 Free:style=Regular:size=12"};
 static const char dmenufont[]       = "monospace:size=16";
 //SourceCode-Bold
+static const unsigned int baralpha = 0xFF*0.7;
+static const unsigned int borderalpha = 0xFF*0.7;
 static const char col_gray1[]       = "#0d0218";
 static const char col_gray2[]       = "#33075f";
 static const char col_gray3[]       = "#d70f73";
@@ -18,9 +27,14 @@ static const char *colors[][3]      = {
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
+};
 
 /* tagging */
-static const char *tags[] = { "", "", "", "", "", "", "", "", "" };
+static const char *tags[] = { "", "", "", "", "", "", ""};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -28,16 +42,13 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",    	 NULL,       NULL,       1 << 6,       0,           -1 },
+	{ "Gimp",    	 NULL,       NULL,       1 << 3,       0,           -1 },
 	{ "firefox",  	 NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "libreoffice", NULL,	     NULL,       1 << 4,       0,           -1 },
-	{ "lmms",        NULL,       NULL,       1 << 7,       0,           -1 },
 	{ "Pavucontrol", NULL,	     NULL,       1 << 5,       0,           -1 },
 	{ "KeePassXC",   NULL,	     NULL,	 1 << 5,       0,	    -1 },	
 	{ "electrum",     NULL,       NULL,       1 << 6,       0,           -1 },
-	{ "nheko",       NULL,       NULL,       1 << 3,       0,           -1 },
-	{ "tor-browser", NULL,       NULL,       1 << 2,       0,           -1 },
-	{ "nuclear",     NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "tor-browser", NULL,       NULL,       1 << 1,       0,           -1 },
 	{ "monero-wallet-gui", NULL, NULL,	 1 << 6,       0,	    -1 },
 };
 
@@ -76,21 +87,20 @@ static const char *ffoxcmd[] =    {"firefox", NULL };
 static const char *liboffcmd[] =  {"libreoffice", NULL};
 static const char *pavucmd[] =    {"pavucontrol", NULL};
 static const char *keepasscmd[] = {"keepassxc", NULL};
-static const char *lmmscmd[] =    {"lmms", NULL};
 static const char *gimpcmd[] =    {"gimp", NULL};
 static const char *electcmd[] =   {"electrum", NULL};
-static const char *nuclcmd[] =    {"nuclear", NULL};
-static const char *nhekocmd[] =   {"nheko", NULL};
 static const char *torcmd[] =     {"tor-browser", NULL};
 static const char *moncmd[] =	  {"monero-wallet-gui", NULL};
 static const char *voltoggle[] =  { "dwm-setvolume", "toggle", NULL };
 static const char *voldown[] = 	  { "dwm-setvolume", "down", NULL };
 static const char *volup[] = 	  { "dwm-setvolume", "up", NULL };
-static const char *sc[] =      	  {"dwm-sc", NULL};
+static const char *mountusb[] =	  { "mount-usb", NULL};
+static const char *umountusb[] =  { "umount-usb", NULL};
+static const char *sc[] =      	  { "dwm-sc", NULL};
 
 static Key keys[] = {
 	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+	{ MODKEY,                       XK_w,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
@@ -99,6 +109,22 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+	{ MODKEY|Mod4Mask,              XK_h,      incrgaps,       {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_l,      incrgaps,       {.i = -1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_h,      incrogaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_l,      incrogaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask|ControlMask,  XK_h,      incrigaps,      {.i = +1 } },
+	{ MODKEY|Mod4Mask|ControlMask,  XK_l,      incrigaps,      {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_0,      togglegaps,     {0} },
+	{ MODKEY|Mod4Mask|ShiftMask,    XK_0,      defaultgaps,    {0} },
+	{ MODKEY,                       XK_y,      incrihgaps,     {.i = +1 } },
+	{ MODKEY,                       XK_o,      incrihgaps,     {.i = -1 } },
+	{ MODKEY|ControlMask,           XK_y,      incrivgaps,     {.i = +1 } },
+	{ MODKEY|ControlMask,           XK_o,      incrivgaps,     {.i = -1 } },
+	{ MODKEY|Mod4Mask,              XK_y,      incrohgaps,     {.i = +1 } },
+	{ MODKEY|Mod4Mask,              XK_o,      incrohgaps,     {.i = -1 } },
+	{ MODKEY|ShiftMask,             XK_y,      incrovgaps,     {.i = +1 } },
+	{ MODKEY|ShiftMask,             XK_o,      incrovgaps,     {.i = -1 } },
 	{ MODKEY,                       XK_Return, zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
@@ -117,19 +143,18 @@ static Key keys[] = {
 	{ MODKEY|ShiftMask,		XK_o,	   spawn, 	   {.v = liboffcmd } },
 	{ MODKEY|ShiftMask,		XK_p,	   spawn,          {.v = pavucmd } },
 	{ MODKEY|ShiftMask,             XK_k,      spawn,          {.v = keepasscmd } },
-	{ MODKEY|ShiftMask,             XK_l,      spawn,          {.v = lmmscmd } },
 	{ MODKEY|ShiftMask,		XK_g,      spawn,          {.v = gimpcmd } },
 	{ MODKEY|ShiftMask,             XK_e,      spawn,          {.v = electcmd } },
 	{ MODKEY|ShiftMask,		XK_t,	   spawn,	   {.v = torcmd } },
-	{ MODKEY|ShiftMask,		XK_n,	   spawn,	   {.v = nuclcmd } },
-	{ MODKEY|ShiftMask,		XK_h,	   spawn,	   {.v = nhekocmd } },
 	{ MODKEY|ShiftMask,		XK_e,	   spawn,	   {.v = moncmd } },
+	{ MODKEY,			XK_x,	   spawn,	   {.v = mountusb } },
+	{ MODKEY|ShiftMask,		XK_x,	   spawn,	   {.v = umountusb } },
 	{ MODKEY,			XK_s,	   spawn,          {.v = sc } },
 	{ MODKEY,			XK_Left,   shiftview,      {.i = -1 } },
 	{ MODKEY,			XK_Right,  shiftview,      {.i = +1 } },
-	{ MODKEY,                       XK_minus,  spawn,          {.v = voltoggle } },
-	{ MODKEY,                       XK_Down,   spawn,          {.v = voldown } },
-	{ MODKEY,                       XK_Up,     spawn,          {.v = volup } },
+	{ 0,	                        XF86XK_AudioMute,	   spawn,          {.v = voltoggle } },
+	{ 0,	                        XF86XK_AudioLowerVolume,   spawn,          {.v = voldown } },
+	{ 0,	                        XF86XK_AudioRaiseVolume,   spawn,          {.v = volup } },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
